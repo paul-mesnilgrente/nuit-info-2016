94b59e9bd7bf940760591f7152a80d29f77cc261
Sageto <maximesanmartin@gmail.com>
2016-11-19 17:29:47 +0100

    Modification de la progress bar

--- web/js/equipe.js
+++ web/js/equipe.js
@@ -35,4 +35,41 @@ $(document).ready(function() {
 		});
 		return false;
 	});
+
+	$('.progress').each(function(){
+		var size = $(this).attr('value');
+		var colori = $(this).attr('color');
+
+		var bar = new ProgressBar.Circle(this, {
+			color: colori,
+			// This has to be the same size as the maximum width to
+			// prevent clipping
+			strokeWidth: 5,
+			trailWidth: 1,
+			easing: 'easeInOut',
+			duration: 1400,
+			text: {
+				autoStyleContainer: false
+			},
+			from: { color: colori, width: 1 },
+			to: { color: colori, width: 4 },
+			// Set default step function for all animate calls
+			step: function(state, circle) {
+				circle.path.setAttribute('stroke', state.color);
+				circle.path.setAttribute('stroke-width', state.width);
+
+				var value = Math.round(circle.value() * size);
+				if (value === 0) {
+					circle.setText('');
+				} else {
+					circle.setText(value);
+				}
+
+			}
+		});
+		bar.text.style.fontSize = '4rem';
+		bar.text.style.color = colori;
+
+		bar.animate(1.0);  // Number from 0.0 to 1.0
+	});
 });
